13490db3bafec245c1b864c839c9d62b5966dc9f
Jay <24zhangjie@gmail.com>
2017-01-05 14:21:21 +0800

    Template Pattern

---
+++ src/template/Sort.java
@@ -0,0 +1,14 @@
+package template;
+
+public class Sort implements Comparable
+{
+
+	@Override
+	public int compareTo(Object o)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
---
+++ src/template/TeaWithHook.java
@@ -0,0 +1,18 @@
+package template;
+
+public class TeaWithHook extends CaffeineBeverageWithHook
+{
+
+	@Override
+	void brew()
+	{
+		System.out.println("Steeping the tea.");
+	}
+
+	@Override
+	void addCondiment()
+	{
+		System.out.println("Adding lemon into teaport.");		
+	}
+
+}
